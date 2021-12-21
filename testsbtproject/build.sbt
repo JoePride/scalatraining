4dd0222648d49c9b55d3145faa2fe392508544da
Joe Pride <jhpride1790@gmail.com>
2021-12-20 19:43:58 -0800

    Added a test Scala project and updated the .gitignore to avoid committing some downloaded files.

---
+++ testsbtproject/build.sbt
@@ -0,0 +1,7 @@
+name := "testsbtproject"
+
+version := "0.1"
+
+scalaVersion := "2.13.7"
+
+idePackagePrefix := Some("com.versanalytics.testsbtproject")
--- testsbtproject/src/main/scala/example/Main.scala
+++ testsbtproject/src/main/scala/example/Main.scala
@@ -1,5 +1,7 @@
-package com.versanalytics.testsbtproject.example
+package com.versanalytics.testsbtproject
+package example
 
-object Main {
-
+object Main extends App {
+  val ages = Seq(12, 49, 4, 18)
+  println(s"The oldest age is ${ages.max}")
 }
